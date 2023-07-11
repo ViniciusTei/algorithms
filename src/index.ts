3520db9940073d296abc820145673d5c58017739
ViniciusTei <viniciustprates@gmail.com>
2023-07-11 19:39:47 -0300

    fix bubblesort

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import { linear_search, binary_search, two_crystal_balls, bubblesort } from './scripts'
 
 function test(name: string, callback: () => boolean) {
-  const result = callback()
-  console.log(name, ' ==> ' ,result ? '\x1b[32m Test passed \x1b[0m' : '\x1b[33m Test rejected') 
+  const resulado = callback()
+  console.log(name, ' ==> ', resulado ? '\x1b[32m Test passed \x1b[0m' : '\x1b[33m Test rejected')
 }
 
-test('should linear search', () => { 
+test('should linear search', () => {
   const a = [1, 2, 3]
   const b = 2
   return linear_search(a, b) === true
 })
 
 
-test('should binary search', () => { 
+test('should binary search', () => {
   const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   const b = 2
   return binary_search(a, 0, a.length, b) === true
@@ -24,9 +24,14 @@ test('should two ball', () => {
   return two_crystal_balls(breaks) === 7
 })
 
-test('shoul bubble sort', () => {
+test('should bubble sort', () => {
   const arr = [1, 3, 7, 4, 2]
   const response = bubblesort(arr)
 
-  
+  for (let i = 0; i < response.length - 1; i++) {
+    if (response[i] > response[i + 1]) {
+      return false
+    }
+  }
+  return true
 })
--- src/scripts/BubbleSort.ts
+++ src/scripts/BubbleSort.ts
@@ -1,8 +1,8 @@
 export default function BubbleSort(array: number[]): number[] {
-  const arr = []
-  array.foreach => a => arr.push(a)
+  let arr: number[] = []
+  array.forEach((a: number) => arr.push(a))
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length - 1; j ++) {
+    for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] > arr[j]) {
         let temp = arr[i]
         arr[i] = arr[j]
